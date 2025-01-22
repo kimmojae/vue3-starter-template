160ed59660306fc75ef4eac116932c9cab5894b4
kimmojae <mojaekim@gmail.com>
2025-01-22 21:22:00 +0900

    add sroll behavior smooth

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import { configureLayouts } from './layoutConfig'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior: () => ({ top: 0, behavior: 'smooth' }),
   routes: setupLayouts(configureLayouts(routes)),
 })
 
